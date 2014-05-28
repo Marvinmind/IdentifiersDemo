bcfa8d2f943846ebf090d34ebc0010ae6a049191
Marvinmind <foaly02@outlook.com>
2014-05-28 02:03:55 +0200

    Stuff added

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,5 @@
-package Domain;
+import Domain.Person;
+import Domain.PersonContainer;
 
 import java.util.Iterator;
 
--- src/Domain/Identifier.java
+++ src/Domain/Identifier.java
@@ -3,6 +3,7 @@ package Domain;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import org.apache.commons.lang3.RandomStringUtils;
 
 /**
  * Created by Martin on 5/27/2014.
@@ -17,24 +18,31 @@ public class Identifier {
         this.name = name;
         this.value = value;
     }
-
-    public void addChild(List<String> nameList, String value) {
+    //Braucht Unterstützung für Algorithmen
+    public void addChild(List<String> nameList) {
         List<Identifier> tempIdentifierList = children;
         Iterator<String> tempStringIterator = nameList.iterator();
         Identifier currentIdentifier = null;
+        String currentName = null;
+        boolean setFlag = false;
         for(int i=0; i<nameList.size()-1; i++){
-            String currentName = tempStringIterator.next();
+            setFlag = false;
+            currentName = tempStringIterator.next();
             Iterator<Identifier> tempIdentifierIterator = tempIdentifierList.iterator();
             while(tempIdentifierIterator.hasNext()){
                 currentIdentifier = tempIdentifierIterator.next();
                 if(currentName == currentIdentifier.name){
-                     tempIdentifierIterator = currentIdentifier.children.iterator();
+                    tempIdentifierList = currentIdentifier.children;
+                    setFlag = true;
                     break;
                 }
-            //Hier logging "Einfügen fehlgeschlagen!"
             }
-            currentIdentifier.children.add(new Identifier(currentName, value);
+            if (setFlag == false){
+            currentIdentifier.children.add(new Identifier(currentName, RandomStringUtils.random(8)));
+            }
         }
+        if(setFlag == true)
+        currentIdentifier.children.add(new Identifier(currentName, RandomStringUtils.random(8));
     }
 
     public static Identifier getChild(Identifier inputIdentifier, String name) {
